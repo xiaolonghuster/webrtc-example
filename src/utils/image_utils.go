c648612bcc36b0484d5f86117d1ec3cdd43e86be
xiaolong <xiaolonghuster@gmail.com>
2025-03-16 00:03:59 +0800

    Update image_utils.go

--- src/utils/image_utils.go
+++ src/utils/image_utils.go
@@ -1,60 +1,106 @@
 package utils
 
 import (
-	"io/ioutil"
+	"github.com/cihub/seelog"
+	"log"
+	"os"
 	"os/exec"
 )
 
 // H264ToJpeg 将H.264帧转换为JPEG格式
-func H264ToJpeg(h264Bytes []byte) ([]byte, error) {
-	// 创建FFmpeg进程
-	cmd := exec.Command("ffmpeg", "-hide_banner", "-loglevel", "error", "-f", "h264", "-i", "pipe:", "-f", "mjpeg", "-")
-	// 将H.264数据传递给FFmpeg进程的标准输入
-	stdin, err := cmd.StdinPipe()
+func H264ToJpeg(h264Frame []byte) ([]byte, error) {
+	seelog.Infof("H264ToJpeg h264Frame length:%d", len(h264Frame))
+	cmd := exec.Command("E:/devtools/ffmpeg/bin/ffmpeg.exe", "-hide_banner", "-loglevel", "error", "-f", "h264", "-i", "pipe:0", "-f", "mjpeg", "pipe:1")
+
+	cmd.Stderr = os.Stderr        // bind log stream to stderr
+	stdin, err := cmd.StdinPipe() // Open stdin pipe
 	if err != nil {
-		return nil, err
+		seelog.Errorf("h264 to jpg cmd stdin pipe err:%v", err)
 	}
-	defer stdin.Close()
 
-	// 执行FFmpeg进程并将输出捕获到变量中
-	stdout, err := cmd.StdoutPipe()
+	resultChan := make(chan []byte)
+
+	go func() {
+		if result, err := cmd.Output(); err == nil {
+			resultChan <- result
+		}
+	}()
+
+	_, err = stdin.Write(h264Frame) // pump audio data to stdin pipe
 	if err != nil {
-		return nil, err
-	}
-	defer stdout.Close()
-	if err := cmd.Start(); err != nil {
-		return nil, err
+		log.Fatalln(err)
 	}
 
-	if _, err := stdin.Write(h264Bytes); err != nil {
-		return nil, err
-	}
-	if err := stdin.Close(); err != nil {
-		return nil, err
-	}
-
-	jpegBytes, err := ioutil.ReadAll(stdout)
+	err = stdin.Close() // close the stdin, or ffmpeg will wait forever
 	if err != nil {
-		return nil, err
+		log.Fatalln(err)
 	}
 
-	if err := cmd.Wait(); err != nil {
-		return nil, err
+	for {
+		select {
+		case data := <-resultChan:
+			return data, nil
+		}
 	}
 
-	return jpegBytes, nil
+	return nil, nil
 }
 
-func main() {
-	// 将H.264帧转换为JPEG
-	h264Bytes := []byte{0x00, 0x00, 0x01, 0x67, 0x42, 0x80, 0x0d, 0xda, 0x38, 0x9b, 0x60}
-	jpegBytes, err := H264ToJpeg(h264Bytes)
-	if err != nil {
-		panic(err)
+func UnpackRTP2H264(rtpPayload []byte) ([]byte, string, byte) {
+	if len(rtpPayload) <= 0 {
+		return nil, "", 0x00
 	}
-
-	// 将JPEG数据写入文件
-	if err := ioutil.WriteFile("/Users/lixiaolong/tmp/20230326/output.jpg", jpegBytes, 0644); err != nil {
-		panic(err)
+	var out []byte
+	fu_indicator := rtpPayload[0]                           //获取第一个字节
+	fu_header := rtpPayload[1]                              //获取第二个字节
+	nalu_type := fu_indicator & 0x1f                        //获取FU indicator的类型域
+	flag := fu_header & 0xe0                                //获取FU header的前三位，判断当前是分包的开始、中间或结束
+	nal_fua := ((fu_indicator & 0xe0) | (fu_header & 0x1f)) //FU_A nal
+	var FrameType string
+	if nal_fua == 0x67 {
+		FrameType = "SPS"
+	} else if nal_fua == 0x68 {
+		FrameType = "PPS"
+	} else if nal_fua == 0x65 {
+		FrameType = "IDR"
+	} else if nal_fua == 0x61 {
+		FrameType = "P Frame61"
+	} else if nal_fua == 0x41 {
+		FrameType = "P Frame41"
 	}
+	seelog.Infof("nalu_type: %x flag: %x FrameType: %s", nalu_type, flag, FrameType)
+	if nalu_type == 0x1c { //判断NAL的类型为0x1c=28，说明是FU-A分片
+		if flag == 0x80 { //分片NAL单元开始位
+			o := make([]byte, len(rtpPayload)+5-2) //I帧开头可能为00 00 00 01、00 00 01，组帧时只用00 00 01开头
+			o[0] = 0x00
+			o[1] = 0x00
+			o[2] = 0x00
+			o[3] = 0x01
+			o[4] = nal_fua
+			copy(o[5:], rtpPayload[2:])
+			//o := make([]byte, len(rtpPayload)+4-2) //I帧开头可能为00 00 00 01、00 00 01，组帧时只用00 00 01开头
+			//o[0] = 0x00
+			//o[1] = 0x00
+			//o[2] = 0x01
+			//o[3] = nal_fua
+			//copy(o[4:], rtpPayload[2:])
+			out = o
+		} else { //中间分片包或者最后一个分片包
+			o := make([]byte, len(rtpPayload)-2)
+			copy(o[0:], rtpPayload[2:])
+			out = o
+		}
+	} else if nalu_type == 0x1 { //单一NAL 单元模式
+		seelog.Infof("----- 单一NAL -----")
+		o := make([]byte, len(rtpPayload)+4) //将整个rtpPayload一起放进去
+		o[0] = 0x00
+		o[1] = 0x00
+		o[2] = 0x00
+		o[3] = 0x01
+		copy(o[4:], rtpPayload[0:])
+		out = o
+	} else {
+		seelog.Infof("Unsport nalu type!")
+	}
+	return out, FrameType, flag
 }
